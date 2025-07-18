7809b11e67cb514321e036e9475577ab07f07288
MalayShikharSoni <malayshikhar2004@gmail.com>
2025-07-18 17:41:19 +0530

    created profileUpdate and checkAuth controllers, set up cloudinary library

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-
+import cloudinary from "../lib/cloudinary.js"
 
 // SIGNUP CONTROLLER
 export const signup = async (req, res) => {
@@ -132,4 +132,48 @@ export const logout = (req, res) => {
     }
 
 };
+
+
+// PFP UPDATE CONTROLLER
+export const updateProfile = async (req, res) => {
+
+    try{
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic) {
+            return res.status(400).json({
+                message: "Profile picture is required"
+            });
+        }
+
+        const uploadResponse =  await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true});
+
+        res.status(200).json({
+            updatedUser
+        });
+        
+    } catch(error) {
+        console.log("Error in profile update: ", error.message);
+        res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+
+}
+
+// CHECK AUTH CONTROLLER
+export const checkAuth = (req, res) => {
+
+    try {
+        res.status(200).json(req.user);
+    } catch(error) {
+        console.log("Error in checkAuth controller", error.message);
+        res. status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+
+}
 
