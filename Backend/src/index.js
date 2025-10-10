c6f292516d9b14259b60d4bc2186d1c71abc3e67
AdityaShuk1a <adityasarika0292004@gmail.com>
2025-10-10 23:39:00 +0530

    rolled back changes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,18 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
-import { createServer } from "http";
 
 import { connectDB } from "./lib/db.js";
-import { initSocket } from "./lib/socket.js"; // ✅ updated import
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
-const server = createServer(app);
-initSocket(server); // ✅ initialize socket.io here
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
@@ -33,12 +31,12 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {  
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 server.listen(PORT, () => {
-  console.log("✅ Server running on PORT:", PORT);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
