25f127359c2b9a2837444d17a1aa81bf0ede91e8
AdityaShuk1a <adityasarika0292004@gmail.com>
2025-10-15 22:21:11 +0530

    fixed socket

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -24,6 +24,7 @@ io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
   const userId = socket.handshake.query.userId;
+  console.log(userId)
   if (userId) userSocketMap[userId] = socket.id;
 
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
